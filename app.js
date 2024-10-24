ea3c9197b7e4cf54bd3abe935387b4fbf58cbc32
Syareeeeeeep <syarifhidayatt81@gmail.com>
2024-10-24 15:07:39 +0700

    selesai

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 function main() {
+  let hasilDadu = [1, 5, 2, 6, 2, 3];
+  let kemuculanGanjil = 0;
+  let kemuculanGenap = 0;
+
+  for(let i=0; i<hasilDadu.length; i++) {
+    // console.log(hasilDadu[i])
+    // if(hasilDadu[i]/2 == 0) {
+    //     kemuculanGenap+=1
+    // }
+    if(hasilDadu[i]%2 == 0) {
+        kemuculanGenap+=1
+    }
+    else {
+        kemuculanGanjil+=1
+    }
+  }
+  console.log(`Raka mendapatkan angka ganjil sebanyak ${kemuculanGanjil} kali dan angka genap sebanyak ${kemuculanGenap} kali`)
   
 
-
 }
+main()
 
 module.exports = main
